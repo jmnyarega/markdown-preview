e641042cc6f7ca50709b9082f79ac7867e9cc8d1
josiahmokob0 <jmnyarega@gmail.com>
2021-11-23 01:38:28 +0300

    - markdown preview

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { marked } from "marked";
+import { useState } from "react";
+
+import "./index.css";
+import "highlight.js/styles/default.css";
+
+const initialMarkdown = `
+ # Hello World!
+ ## This is a beauty
+ [link](https://www.google.com?q=markdown)
+ \`code\`
+ \`\`\`
+  const x = 90;
+ \`\`\`
+
+ - one
+ - two
+ - three
+
+ > quote from a smart person
+
+ ![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+
+ **bold text**
+`;
 
 function App() {
+  const [text, setText] = useState(initialMarkdown);
+
+  marked.setOptions({
+    breaks: true,
+    gfm: true,
+    highlight: function (code, lang) {
+      const hljs = require("highlight.js");
+      const language = hljs.getLanguage(lang) ? lang : "javascript";
+      return hljs.highlight(code, { language }).value;
+    },
+  });
+
+  const markdown = marked.parse(text || "");
+
+  const onChange = (e) => setText(e.target.value);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="container">
+      <h3 className="title"> Markdown Previewer </h3>
+      <textarea id="editor" value={text} onChange={onChange} />
+      <div id="preview" dangerouslySetInnerHTML={{ __html: markdown }} />
+    </main>
   );
 }
 
